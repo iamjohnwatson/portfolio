22d65b0de03267b8fc97286d485eae3061b018fc
Akash Sriram <46345994+iamjohnwatson@users.noreply.github.com>
2025-03-14 08:29:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -152,7 +152,6 @@ document.addEventListener('DOMContentLoaded', function () {
             ScrollTrigger.create({
                 trigger: element,
                 start: 'top 80%',
-                end: 'bottom top',
                 onEnter: () => {
                     animation.play();
                     element.classList.add('is-visible');
@@ -173,7 +172,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 scrollTrigger: {
                     trigger: section,
                     start: 'top bottom',
-                    end: 'bottom top', // Changed to 'bottom bottom' to reduce scroll height
+                    end: 'bottom bottom', // Changed to 'bottom bottom' to reduce scroll height
                     scrub: true
                 }
             });
